acad7de986980495d78f9f4f360af873718d0737
Cameron Jones <jones_cameron@student.mahoningctc.com>
2024-11-06 11:57:11 -0500

    Add project files.

---
+++ basic operations on numbers in C#/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace basic_operations_on_numbers_in_C_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int fahrenheit = 94;
+            decimal cel = (fahrenheit - 32m) * (5m / 9m);
+
+            Console.WriteLine("The temp is " + cel + " in celsius");
+
+            Console.ReadLine();
+        }
+    }
+}
